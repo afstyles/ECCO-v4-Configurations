150ac951bf24d7f8b9e554e3601fba1f0c20b1d6
Andrew Styles <andrew.styles@linacre.ox.ac.uk>
2025-12-18 12:52:46 +0000

    State of ECCO for Basin-wide geostrophic publication. Switched on bottom friction control for future work

--- ECCOv4 Release 4/code/CTRL_OPTIONS.h
+++ ECCOv4 Release 4/code/CTRL_OPTIONS.h
@@ -44,6 +44,9 @@ C This allows for initila UVEL and VVEL controls
 #define ALLOW_UVEL0_CONTROL
 #define ALLOW_VVEL0_CONTROL
 
+C This allows for BOTTOMDRAG controls
+#define ALLOW_BOTTOMDRAG_CONTROL
+
 C This allosws updating etaH as well.
 #define ALLOW_ETAH_UPDATE
 
