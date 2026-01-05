b9e7c641157f8b5581bc8f380ad504b9672d8dce
Andrew Styles <andrew.styles@linacre.ox.ac.uk>
2026-01-05 16:40:36 +0000

    Updated CTRL_SIZE to accommodate nlbottomdrag control field

--- ECCOv4 Release 4/code/CTRL_SIZE.h
+++ ECCOv4 Release 4/code/CTRL_SIZE.h
@@ -16,7 +16,7 @@ C     maxCtrlTim2D :: number of 2-d generic tim-varying ctrl variables
 C     maxCtrlProc  :: number of pre-processing options per ctrl variable
 
       integer     maxCtrlArr2D
-      parameter ( maxCtrlArr2D = 2 )
+      parameter ( maxCtrlArr2D = 3 )
 
       integer     maxCtrlArr3D
       parameter ( maxCtrlArr3D = 7 )
